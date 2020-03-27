17e98f363454e1c5a1e819e544301a11344e7d09
The-X-Team-Debug <61815823+The-X-Team-Debug@users.noreply.github.com>
2020-03-27 18:45:33 +0000

    Update brute_zipfile.py

--- brute_zipfile.py
+++ brute_zipfile.py
@@ -1,10 +1,14 @@
 import zipfile
 from tqdm import tqdm
 
-# Tarus sekumpulan file yg sudah diisi dengan password ber ektensi txt
-sora = "word.txt"
-# Disini adalah file yg ingin di brute ingat taruh file nya di folder ini
-shiro = "TheXTools.zip"
+# Tarus sekumpulan file yg sudah diisi
+# dengan password ber ektensi txt
+sora = "worldlist.txt"
+# Disini adalah file yg ingin di brute
+# ingat taruh file nya di folder ini
+# ganti nilai shiro dengan file yg akan
+# di brute
+shiro = "file yg ingin di brute.zip"
 
 shiro = zipfile.ZipFile(shiro)
 kuhaku = len(list(open(sora, 'rb')))
@@ -20,6 +24,6 @@ with open(sora, 'rb') as sora:
             print("Pw ketemu !!:",TheX.decode().strip())
             exit(0)
 print("Pw gk ketemu:")
-print("Silahkan isi worldlist nya dengan keyakinan anda :)")
+print("Silahkan isi worldlist nya dengan pw yg tepat :)")
 
 
